9463149239d67c33579d015c9485631de59bca31
marcoche88 <marco.che@hotmail.it>
2021-09-28 15:39:22 +0200

    added ex-2

--- university.sql
+++ university.sql
@@ -2,3 +2,8 @@
 SELECT * 
 FROM `students` 
 WHERE YEAR(`date_of_birth`) = "1990";
+
+-- 2. Selezionare tutti i corsi che valgono più di 10 crediti
+SELECT `name`, `cfu` 
+FROM `courses` 
+WHERE `cfu` > 10;
