f53fceac4a090fa226b66e00f495a8df6ce22ac6
marcoche88 <marco.che@hotmail.it>
2021-09-28 15:58:47 +0200

    added ex-3

--- university.sql
+++ university.sql
@@ -7,3 +7,8 @@ WHERE YEAR(`date_of_birth`) = "1990";
 SELECT `name`, `cfu` 
 FROM `courses` 
 WHERE `cfu` > 10;
+
+-- 3. Selezionare tutti gli studenti che hanno più di 30 anni
+SELECT `name`, `surname`, `date_of_birth`
+FROM `students` 
+WHERE (YEAR(NOW()) - YEAR(`date_of_birth`)) > 30;
