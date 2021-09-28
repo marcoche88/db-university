da1d43300c0e493316d60e5dedb4fae5e9881fc6
marcoche88 <marco.che@hotmail.it>
2021-09-28 16:04:41 +0200

    added ex-4

--- university.sql
+++ university.sql
@@ -12,3 +12,8 @@ WHERE `cfu` > 10;
 SELECT `name`, `surname`, `date_of_birth`
 FROM `students` 
 WHERE (YEAR(NOW()) - YEAR(`date_of_birth`)) > 30;
+
+-- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea
+SELECT *
+FROM `courses` 
+WHERE `degree_id` = 2 AND `year` = 1 AND `period` = "I semestre";
