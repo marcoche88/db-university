129b89be94ff06c010ec2886c0f1325e6c12fd00
marcoche88 <marco.che@hotmail.it>
2021-09-28 16:17:31 +0200

    added ex-8

--- university.sql
+++ university.sql
@@ -31,3 +31,8 @@ WHERE `level` = "magistrale";
 -- 7. Da quanti dipartimenti è composta l'università?
 SELECT COUNT(*) AS "num_departments"
 FROM `departments`;
+
+-- 8. Quanti sono gli insegnanti che non hanno un numero di telefono?
+SELECT COUNT(*) AS "tot_teachers_no_phone" 
+FROM `teachers` 
+WHERE `phone` IS NULL;
