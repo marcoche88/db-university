03f363e6aca6d35f24dab2f3b0310c5f49bc9297
marcoche88 <marco.che@hotmail.it>
2021-09-29 15:31:59 +0200

    added ex-2 join

--- university_join.sql
+++ university_join.sql
@@ -4,3 +4,10 @@ FROM `students`
 JOIN `degrees`
 ON `degrees`.`id` = `students`.`degree_id`
 WHERE `degrees`.`name` = "Corso di Laurea in Economia";
+
+-- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
+SELECT `degrees`.`name`, `departments`.`name` 
+FROM `degrees`
+JOIN `departments`
+ON `departments`.`id` = `degrees`.`department_id`
+WHERE `departments`.`name` = "Dipartimento di Neuroscienze";
