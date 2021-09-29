341dc152e241377f1a336428763a9772b0bcaddc
marcoche88 <marco.che@hotmail.it>
2021-09-29 16:04:08 +0200

    added ex-6 join

--- university_join.sql
+++ university_join.sql
@@ -40,3 +40,12 @@ ON `courses`.`id` = `course_teacher`.`course_id`
 JOIN `teachers`
 ON `teachers`.`id` = `course_teacher`.`teacher_id`
 ORDER BY `degrees`.`name`;
+
+-- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica
+SELECT DISTINCT `teachers`.`name`, `teachers`.`surname`, `departments`.`name` 
+FROM `course_teacher`
+JOIN `teachers` ON `teachers`.`id` = `course_teacher`.`teacher_id`
+JOIN `courses` ON `courses`.`id` = `course_teacher`.`course_id`
+JOIN `degrees` ON `degrees`.`id` = `courses`.`degree_id`
+JOIN `departments` ON `departments`.`id` = `degrees`.`department_id`
+WHERE `departments`.`name` = "Dipartimento di Matematica";
